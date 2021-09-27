004fe381e33ebef70952fd76b0bbb3ffdb0d633f
safiirarahmahlinisa-2017051015 <safiira.rahmahlinisa2015@students.unila.ac.id>
2021-09-27 16:59:53 +0700

    Class Individu dan Perusahaan

---
+++ Final Project/src/pkgfinal/project/Individu.java
@@ -0,0 +1,48 @@
+package pkgfinal.project;
+
+import java.util.ArrayList;
+
+public class Individu extends Nasabah {
+
+    private long nik;
+    private long npwp;
+
+    public Individu(long nip, long npwp, String nama, String alamat, ArrayList<Rekening> rekening){
+        super(nama, alamat, rekening);
+        this.nik = nip;
+        this.npwp = npwp;
+    }
+
+    public long getNpwp() {
+        return npwp;
+    }
+
+    public void setNpwp(long npwp) {
+        this.npwp = npwp;
+    }
+
+    public long getNik() {
+        return nik;
+    }
+
+    public void setNik(long nik) {
+        this.nik = nik;
+    }
+    
+    @Override
+    public void print() {
+        System.out.println("Nama: "+nama);
+        System.out.println("Alamat: "+alamat);
+        System.out.println("NIK: "+nik);
+        System.out.println("NPWP: "+npwp);
+        System.out.println("===========================================");
+        System.out.println("No Rekening                     Saldo");
+        System.out.println("===========================================");
+        
+        for(int i=0;i<rekening.size();i++){
+            System.out.printf("%d  %32.2f\n", rekening.get(i).getNoRekening(),rekening.get(i).getSaldo());
+        }
+
+    }
+    
+}
